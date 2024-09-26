5208924756a83a81e3401212c18a37a14f43709b
dadahon <dadahon@gmail.com>
2024-09-26 10:52:17 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,133 @@
+// Bolean 1
+// let a = prompt("a sonini kiriting");
+// if (a > 0) {
+//   console.log("musbat son");
+// } else {
+//   console.log("musbat son emas");
+// }
+// Bolean 2
+// let a = prompt("a sonini kiriting");
+// if (a % 2 == 1) {
+//     console.log("a toq son");
+// } else {
+//     console.log("a toq son emas");
+// }
+// Bolean 3
+// let a = prompt("a sonini kiriting");
+// if (a % 2 == 0) {
+//     console.log("a juft son");
+// } else {
+//     console.log("a juft son emas");
+// }
+// Bolean 4
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if (a > 2 && b <= 3) {
+//     console.log("rost");
+// } else {
+//     console.log("yolg'on");
+// }
+// Bolean 5
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if (a >= 0 || b < -2) {
+//     console.log("rost");
+// } else {
+//     console.log("yolg'on");
+// }
+// Bolean 6
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+// if (a <= b && b <= c) {
+//     console.log("rost");
+// } else {
+//     console.log("yolg'on");
+// }
+// Bolean 7
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+// if (a < b && b < c) {
+//     console.log("rost");
+// } else {
+//     console.log("yolg'on");
+// }
+// Bolean 8
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if (a % 2 == 1 && b % 2 == 1) {
+//     console.log("a va b toq son");
+// } else {
+//     console.log("a yoki/va b toq son emas");
+// }
+// Bolean 9
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if (a % 2 == 1 || b % 2 == 1) {
+//     console.log("a yoki b toq son");
+// } else {
+//     console.log("a yoki b toq son emas");
+// }
+// Bolean 10
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiritng");
+// if ((a % 2 == 1 && b % 2 == 0) || (a % 2 == 0 && b % 2 == 1)) {
+//     console.log("rost (a va b ning faqat 1tasi toq son)");
+// } else {
+//     console.log("yolg'on (a va b ning 2lasi ham juft yoki toq sonlar)");
+// }
+// Bolean 11
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiritng");
+// if ((a % 2 == 1 && b % 2 == 1) || (a % 2 == 0 && b % 2 == 0)) {
+//     console.log("rost (a va b ning 2lasi ham toq yoki juft sonlar)");
+// } else {
+//     console.log("yolg'on (a va b ning 1tasi juft yoki toq son)");
+// }
+// Bolean 12
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+// if (a > 0 && b > 0 && c > 0) {
+//     console.log("rost (barchasi musbat)");
+// } else {
+//     console.log("yolg'on (barchasi musbat emas)");
+// }
+// Bolean 13
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+// if (a > 0 || b > 0 || c > 0) {
+//     console.log("rost (kamida 1tasi musbat)");
+// } else {
+//     console.log("yolg'on (1tasi ham musbat emas)");
+// }
+// Bolean 14
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+
+// if (
+//     (a > 0 && b < 0 && c < 0) ||
+//     (a < 0 && b > 0 && c < 0) ||
+//     (a < 0 && b < 0 && c > 0)
+// ) {
+//     console.log("rost");
+// } else {
+//     console.log("yolg'on");
+// }
+// Bolean 15
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+
+// if (
+//     (a > 0 && b > 0 && c < 0) ||
+//     (a > 0 && b < 0 && c > 0) ||
+//     (a < 0 && b > 0 && c > 0)
+// ) {
+//     console.log("rost");
+// } else {
+//     console.log("yolg'on");
+// }
